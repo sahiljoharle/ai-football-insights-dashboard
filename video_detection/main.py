07203f0ff9783b20c43b629bf8657c45c271cc5d
Sahil Joharle <sahil@naveraventures.com>
2026-05-20 00:33:21 +0530

    Adding tracking  abilities

--- video_detection/main.py
+++ video_detection/main.py
@@ -1,11 +1,17 @@
 from utils import read_video,save_video
+from trackers import Tracker
 
 def main():
     #read Video
     video_frames = read_video('input_videos/08fd33_4.mp4')
 
+    #initialize Tracker
+    tracker = Tracker('models/best.pt')
+
+    tracks = tracker.get_object_tracks(video_frames)
+
     #save Video
-    save_video(video_frames, 'output_video/output_videos.mp4')
+    save_video(video_frames, 'output_video/output_videos.avi')
 
 if __name__ == "__main__":
     main()
--- video_detection/utils/video_utils.py
+++ video_detection/utils/video_utils.py
@@ -11,7 +11,7 @@ def read_video(video_path):
     return frames
 
 def save_video(output_video_frames, output_video_path):
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
     out = cv2.VideoWriter(output_video_path, fourcc, 24, (output_video_frames[0].shape[1], output_video_frames[0].shape[0]))
     for frame in output_video_frames:
         out.write(frame)
